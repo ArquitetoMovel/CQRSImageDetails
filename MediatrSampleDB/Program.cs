f333379a9853306d322b91a6f080445ba03e2e61
Alexandre <alexandredanelon@outlook.com>
2020-06-12 21:31:26 -0300

    v0.1

---
+++ MediatrSampleDB/Program.cs
@@ -0,0 +1,28 @@
+﻿using MediatrSampleDB.Repository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatrSampleDB
+{
+    class Program
+    {
+        //static async Task Main(string[] args)
+        //{
+
+        //}
+
+        static void Main(string[] args)
+        {
+            var repo = new DBPostgres();
+            foreach (var item in new DirectoryInfo(@"D:\Fotos Pai").GetFiles("*.jpg"))
+            {
+                repo.InsertImages(new Commands.CreateNewImageCommand { Name = item.Name, Path = item.FullName });
+            }
+
+        }
+    }
+}
---
+++ MediatrSampleDB/Repository/DBPostgres.cs
@@ -0,0 +1,70 @@
+﻿using MediatrSampleDB.Commands;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatrSampleDB.Repository
+{
+    public class DBPostgres 
+    {
+        private readonly NpgsqlConnection _pgdbConn;
+        public DBPostgres()
+        {
+            var connString = "Host=localhost;Username=postgres;Password=dbtest1;Database=postgres";
+            _pgdbConn= new NpgsqlConnection(connString);
+        }
+
+        public bool InsertImages(CreateNewImageCommand createNewImageCommand)
+        {
+ 
+            var commit = true;
+            try
+            {
+                _pgdbConn.Open();
+                // next id
+                var nextId = 0;
+                using (var cmd = new NpgsqlCommand("SELECT count(1) FROM tb_images", _pgdbConn))
+                {
+                    try
+                    {
+                        nextId = Int32.Parse(cmd.ExecuteScalar().ToString())+1;
+                    }
+                    catch(NpgsqlException)
+                    {
+                        commit = false;
+                    }
+                    catch (Exception e)
+                    {
+                        nextId = 1;
+                    }
+                  
+                }
+                try
+                {
+                    // Insert some data
+                    using (var cmd = new NpgsqlCommand("INSERT INTO tb_images (id,name,path) VALUES (@id,@name,@path)", _pgdbConn))
+                    {
+                        cmd.Parameters.AddWithValue("id", nextId);
+                        cmd.Parameters.AddWithValue("name", createNewImageCommand.Name);
+                        cmd.Parameters.AddWithValue("path", createNewImageCommand.Path);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (NpgsqlException)
+                {
+                    commit = false;
+                }
+                
+            }
+            finally
+            {
+                _pgdbConn.Close();
+            }
+            return commit;
+        }
+    }
+}
